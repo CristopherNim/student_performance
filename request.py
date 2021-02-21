2dd0762ee1fc26895ae20cbf7a391b17d6ffcc32
CristopherNim <74469204+CristopherNim@users.noreply.github.com>
2021-02-21 18:40:02 -0500

    Add files via upload

---
+++ request.py
@@ -0,0 +1,11 @@
+# import requests
+#
+# url = 'http://localhost:5000/results'
+#
+# r = requests.post(url, json={'gender': 'female', 'race': 'group E',
+#                              'parental_level_of_education': 'some high school', 'lunch': 'standard',
+#                              'test_preparation_course': 'none', 'reading_score': 80,
+#                              'writing_score': 80})
+#
+# print(r.json())
+#
